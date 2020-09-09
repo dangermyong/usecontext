23ec1e0658074cfdb0d2e496fb644a3493d7ad07
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-09 22:14:31 +0900

    typing

---
+++ src/FunctionContextComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FunctionContextComponent() {
+  return (
+    <div>
+      <div>Function Theme</div>
+    </div>
+  )
+}
+
+export default FunctionContextComponent
